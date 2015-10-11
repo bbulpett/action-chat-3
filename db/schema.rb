1bd4ce54fb2b3aa764349014764bc53fc31ae47b
Barnabas Bulpett <bbulpett@charter.net>
2015-10-11 11:26:38 -0400

    Users controller and a better user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151011151106) do
+ActiveRecord::Schema.define(version: 20151011152049) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "username"
+    t.string   "email"
+    t.string   "password_hash"
+    t.string   "password_salt"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
 end
